b83175da4a6172af8b6eaff6cb3971713f43fce2
Brian McGillion <brian@ssrc.tii.ae>
2022-04-09 13:15:47 +0400

    Initial copy from hlissner-dotfiles
    Signed-off-by: Brian McGillion <brian@ssrc.tii.ae>

---
+++ hosts/hades/default.nix
@@ -0,0 +1,77 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      browsers = {
+        default = "firefox";
+        firefox.enable = true;
+      };
+      media = {
+        documents.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+      vm = {
+        qemu.enable = true;
+      };
+    };
+    dev = {
+      rust.enable = false;
+      python.enable = true;
+    };
+    editors = {
+      default = "emacs";
+      emacs.enable = true;
+      vim.enable = false;
+    };
+    shell = {
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+    };
+    services = {
+      ssh.enable = false;
+      docker.enable = true;
+      # Needed occasionally to help the parental units with PC problems
+      # teamviewer.enable = true;
+    };
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = false;
+
+  networking.networkmanager.enable = true;
+  networking.hostName = "hades"; # Define your hostname
+  
+  # The global useDHCP flag is deprecated, therefore explicitly set to false
+  # here. Per-interface useDHCP will be mandatory in the future, so this
+  # generated config replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Set your time zone.
+  time.timeZone = "Asia/Dubai";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+}
